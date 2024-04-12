1fbbb061cbbaee7dc7aacab9ea02565939b77950
RyanKim-2117 <ghkstjdrne@gmail.com>
2024-04-12 16:05:45 +0900

    usercheck version 1.0

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function whosPaying() {
+    let member = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+    let randomIndex = Math.floor(Math.random() * (member.length))
+ console.log (member[randomIndex] + " is going to buy lunch today!")
+}
+
+whosPaying()
